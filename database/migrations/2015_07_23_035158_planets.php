c4e8c3275cdff277d94c3e3272d501404321d68a
Christian Pschorr <pschorr.christian@gmail.com>
2015-07-23 06:15:15 +0200

    add planet database migration

---
+++ database/migrations/2015_07_23_035158_planets.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Planets extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('planets', function (Blueprint $table) {
+			$table->increments('id');
+			$table->timestamps();
+		});
+
+		Schema::table('planets', function (Blueprint $table) {
+			$table->string('name')->nullable();
+			$table->integer('map');
+			$table->integer('x');
+			$table->integer('y');
+			$table->integer('z');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('planets');
+	}
+}
